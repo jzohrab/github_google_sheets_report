e25e0f45425564d03b546b15f757b90ef209251d
Jeff Zohrab <jzohrab@gmail.com>
2017-10-23 16:33:51 -0400

    wip

--- GitHubRepoAnalyzer/branch_report.py
+++ GitHubRepoAnalyzer/branch_report.py
@@ -21,3 +21,14 @@ cmd = "git fetch {origin}".format(origin = config[':origin_name'])
 if (config[':do_fetch']):
     print("Fetching ...")
     subprocess.run(cmd, shell=True)
+cmd = "git remote prune {origin}".format(origin = config[':origin_name'])
+if (config[':do_prune']):
+    print("Pruning ...")
+    subprocess.run(cmd, shell=True)
+
+cmd = "git branch -r"
+result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+lines = result.stdout.decode().split("\n")
+lines = [line.strip() for line in lines if "HEAD" not in line]
+for line in iter(lines):
+    print(line)
