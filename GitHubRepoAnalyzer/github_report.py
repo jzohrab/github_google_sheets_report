d603c65b19688e12e85ceecdbdfc951f3104d549
Jeff Zohrab <jzohrab@gmail.com>
2017-11-02 17:53:29 -0400

    wip combine data

--- GitHubRepoAnalyzer/github_report.py
+++ GitHubRepoAnalyzer/github_report.py
@@ -11,12 +11,24 @@ class GitHubReport:
         self.github_api = github_api
 
     def build_report(self):
-        data = GitBranches(self.config, self.git_repo).load_data()
-        print(json.dumps(data, indent=2, sort_keys=True))
+        git_branches = GitBranches(self.config, self.git_repo).load_data()
+        prs = GitHubPullRequests(self.config, self.github_api).load_data()
+        branch_dict = { b['branch_name']:b for b in git_branches }
+        pr_dict = { pr['branch']:pr for pr in prs }
 
-        pr = GitHubPullRequests(self.config, self.github_api)
-        print(json.dumps(pr.load_data(), indent=2, sort_keys=True))
-
+        full = {branch_name: dict(list(branch_dict[branch_name].items()) + list(pr_dict.get(branch_name, {}).items())) for branch_name, data in branch_dict.items()}
+        data = list(full.values())
+        # print(data)
+        # return
+        def final_data(d):
+            def get_joined(key):
+                return ', '.join(d.get(key, []) or [])
+            d['authors'] = get_joined('authors')
+            d['approved'] = get_joined('approved')
+            d['declined'] = get_joined('declined')
+            return d
+        data = list(map(final_data, data))
+        print(data)
 
 def build_report():
     def get_valid_env_var(name):
