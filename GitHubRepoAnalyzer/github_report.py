081fd27115486bcf99132e4ce8ce443feb64d0b4
Jeff Zohrab <jzohrab@gmail.com>
2017-11-02 16:54:25 -0400

    wip

--- GitHubRepoAnalyzer/github_report.py
+++ GitHubRepoAnalyzer/github_report.py
@@ -4,26 +4,40 @@ import yaml
 import json
 import os
 
+class GitHubReport:
+    def __init__(self, config, git_repo, github_api):
+        self.config = config
+        self.git_repo = git_repo
+        self.github_api = github_api
+
+    def build_report(self):
+        data = GitBranches(self.config, self.git_repo).load_data()
+        print(json.dumps(data, indent=2, sort_keys=True))
+
+        pr = GitHubPullRequests(self.config, self.github_api)
+        print(json.dumps(pr.load_data(), indent=2, sort_keys=True))
+
+
 def build_report():
+    def get_valid_env_var(name):
+        ret = os.environ[name]
+        if (ret is None or ret.strip() == ''):
+            print("Missing {name} env variable".format(name = name))
+            sys.exit()
+        return ret
+
     config = None
     with open('config.yml', 'r') as f:
         config = yaml.load(f)
-    dirname = config['localclone']['source_dir']
-    data = GitBranches(config, GitRepo(dirname)).load_data()
-    print(json.dumps(data, indent=2, sort_keys=True))
 
-    token=os.environ['GITHUB_TOKEN']
-    account=os.environ['GITHUB_TOKEN_ACCOUNT']
-    if (token is None or token.strip() == ''):
-        print("Missing GITHUB_TOKEN env variable")
-        sys.exit()
-    if (account is None or account.strip() == ''):
-        print("Missing GITHUB_TOKEN_ACCOUNT env variable")
-        sys.exit()
+    git_repo = GitRepo(config['localclone']['source_dir'])
 
-    api = GitHubApi(account, token)
-    pr = GitHubPullRequests(config, api)
-    print(json.dumps(pr.load_data(), indent=2, sort_keys=True))
+    token = get_valid_env_var('GITHUB_TOKEN')
+    account = get_valid_env_var('GITHUB_TOKEN_ACCOUNT')
+    github_api = GitHubApi(account, token)
+
+    ghr = GitHubReport(config, git_repo, github_api)
+    ghr.build_report()
 
 if __name__ == '__main__':
     build_report()
