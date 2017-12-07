cb234270632966f11b15dfb0ca25841290de6833
Jeff Zohrab <jzohrab@gmail.com>
2017-12-07 07:40:21 -0500

    wip restructure

--- GitHubReport/report.py
+++ GitHubReport/report.py
@@ -1,4 +1,4 @@
-from GitHubReport.github import GitHubApi, GitHubPullRequests
+from .github import GitHubApi, GitHubPullRequests
 
 import argparse
 import yaml
@@ -73,30 +73,3 @@ def create_report(config, github_creds):
     data = pandas.merge(df, github_user_to_team_map(config), how='left', left_on=['user'], right_on=['github_user'])
     data.fillna(value='', inplace=True)
     dump_dataframe('pull_reqs', data)
-
-
-def parse_arguments():
-    """Get command line args."""
-    parser = argparse.ArgumentParser(description='Generate google sheets from github data.')
-    parser.add_argument(
-        '--config', dest='configfile', default='config.yml',
-        help='Path to config file (default is config.yml)'
-    )
-    parser.add_argument(
-        '--creds', dest='githubcreds', default='github_creds.yml',
-        help='Path to github credentials file (default is github_creds.yml)'
-    )
-    args = parser.parse_args()
-    return args
-
-if __name__ == '__main__':
-    args = parse_arguments()
-
-    def get_yml(yml_path):
-        with open(yml_path, 'r') as f:
-            return yaml.load(f)
-
-    config = get_yml(args.configfile)
-    github_creds = get_yml(args.githubcreds)
-
-    create_report(config, github_creds)
